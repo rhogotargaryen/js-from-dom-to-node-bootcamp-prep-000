3d043e6dd8fc3acbf38b39638cd64f61f771895f
Derek Haller <dhaller.personal@gmail.com>
2017-09-14 19:02:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // write your code below!
-console.log("Hello, World!")
+var name = "Joe"
+var height = "74"
+
+if (name === "Susan") {
+  console.log( "The name is correct")
+} else {
+  console.log ("Expected the name Susan")
+}
